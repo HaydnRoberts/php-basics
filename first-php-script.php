ab2a0c7adb0312c19849db076b98402bbc0c0878
HaydnRoberts <hr342208@truro-penwith.ac.uk>
2023-10-20 10:08:55 +0100

    Update first-php-script.php

--- first-php-script.php
+++ first-php-script.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html> 
 <html> 
-<head> 
-    <title>Using Echo</title> 
-</head> 
-<body> 
-    <p>This is standard HTML</p> 
-    <?php echo 'This was generated using PHP!'; ?> 
+    <head> 
+        <title>Using Echo</title> 
+    </head> 
+    <body> 
+        <p>This is standard HTML</p> 
+        <?php echo 'This was generated using PHP!'; echo '<h1>PHP & HTML</h1><p>PHP can be used to generate HTML.</p>'; ?>
 </body> </html>
