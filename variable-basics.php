59c5b61224c1e4f2d2b0c24beb7c8b407f21fe68
HaydnRoberts <hr342208@truro-penwith.ac.uk>
2023-10-20 10:34:03 +0100

    worksheets 2 and 3

---
+++ variable-basics.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html> 
+<html> 
+    <head> 
+    <title>PHP Variables</title> 
+    </head> 
+    <body> 
+        <?php $firstName = 'Jeff'; 
+        $lastName = 'Jones'; 
+        $fullName = $firstName . ' ' . $lastName; 
+        echo '<p>Hello, <strong>' . $fullName . '</strong> welcome back.<br />It is good to see you again.</p>'; 
+        ?> 
+    </body>
+</html>
